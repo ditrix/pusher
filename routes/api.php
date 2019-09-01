06fb92bdb0f255b2045f5336e8868a8c2393ff77
ditrix <ditrix2005@gmail.com>
2019-09-01 18:12:02 +0300

    preset react npm install  models controllers  route-api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// routes/api.php
+
+    Route::get('projects', 'ProjectController@index');    // получить список проектов
+    Route::post('projects', 'ProjectController@store');   // добавить проект
+    Route::get('projects/{id}', 'ProjectController@show'); // получитть проект 
+    Route::put('projects/{project}', 'ProjectController@markAsCompleted'); // пометить как выполненый
+    Route::post('tasks', 'TaskController@store');                  // добавить задание
+    Route::put('tasks/{task}', 'TaskController@markAsCompleted');  // поменить задание как выполненое
